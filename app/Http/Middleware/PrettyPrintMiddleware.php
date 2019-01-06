c14cda3ed94412fa49b7e8ea204b57a94249cac0
Francesco Zanoni <francescozanoni81@gmail.com>
2019-01-06 10:10:51 +0100

    added pretty print middleware

---
+++ app/Http/Middleware/PrettyPrintMiddleware.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+
+/**
+ * Class PrettyPrintMiddleware
+ * @package App\Http\Middleware
+ */
+class PrettyPrintMiddleware
+{
+
+  /**
+   * Apply pretty print if designated
+   * 
+   * @param $request
+   * @param \Closure $next
+   * @return mixed
+   */
+  public function handle($request, \Closure $next)
+  {
+  
+    $response = $next($request);
+    
+    if (in_array(env('APP_ENV'), ['local', 'testing']) === true) {
+    
+    // https://www.aaronsaray.com/2017/laravel-pretty-print-middleware
+    if ($response instanceof JsonResponse) {
+        $response->setEncodingOptions(JSON_PRETTY_PRINT);
+    }
+    if ($response instanceof Response &&
+        $response->headers->get('Content-Type') === 'application/json') {
+        $content = $response->getContent();
+        $content = json_decode($content);
+        $content = json_encode($content, JSON_PRETTY_PRINT);
+        $response->setContent($content);
+    }
+    
+    }
+        
+    return $response;
+  }
+  
+}
