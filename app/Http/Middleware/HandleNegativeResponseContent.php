b41f0d79dbf4c67de958d3f6dc41961baf2b1ee5
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2019-01-16 11:36:35 +0100

    improved OpenAPI schema

--- app/Http/Middleware/HandleNegativeResponseContent.php
+++ app/Http/Middleware/HandleNegativeResponseContent.php
@@ -25,14 +25,14 @@ class HandleNegativeResponseContent
 
         $response = $next($request);
 
-        // Set failed validation messages as response content.
+        // Set failed validation messages as response content and 400 as status code.
         if ($response->exception &&
             $response->exception instanceof ValidationException) {
             $response->setContent($response->exception->validator->errors()->toArray());
             $response->setStatusCode(400);
         }
 
-        // In case of NOT FOUND, empty content.
+        // In case of NOT FOUND, remove any content.
         if ($response->isNotFound() === true) {
             $response->setContent('');
         }
