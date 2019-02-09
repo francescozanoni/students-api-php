703ab103b7f288d99eda8eee067ef4b70edb93a1
Francesco Zanoni <francescozanoni81@gmail.com>
2019-02-09 18:04:08 +0100

    seminar attendances: added getById test

--- tests/SeminarAttendancesTest.php
+++ tests/SeminarAttendancesTest.php
@@ -28,12 +28,79 @@ class SeminarAttendancesTest extends TestCase
                             'last_name' => 'Doe',
                             'e_mail' => 'john.doe@foo.com',
                             'phone' => '1234-567890',
-                            'nationality' => 'GB'
-                        ]
-                    ]
-                ]
+                            'nationality' => 'GB',
+                        ],
+                    ],
+                ],
             ])
             ->seeStatusCode(200);
+
+    }
+    
+    /**
+     * Get seminar attendance by ID.
+     */
+    public function testGetById()
+    {
+
+        // Existing
+        $this->json('GET', '/seminar_attendances/1')
+            ->seeJsonEquals([
+                'status_code' => 200,
+                'status' => 'OK',
+                'message' => 'Resource successfully retrieved/created/modified',
+                'data' => [
+                    'id' => 1,
+                    'seminar' => 'First seminar',
+                    'start_date' => '2019-01-08',
+                    'end_date' => '2019-01-09',
+                    'ects_credits' => 1.2,
+                    'student' => [
+                        'id' => 1,
+                        'first_name' => 'John',
+                        'last_name' => 'Doe',
+                        'e_mail' => 'john.doe@foo.com',
+                        'phone' => '1234-567890',
+                        'nationality' => 'GB',
+                    ],
+                ],
+            ])
+            ->seeStatusCode(200);
+
+    }
+    
+    /**
+     * Get seminar attendance by ID: failure.
+     */
+    public function testGetByIdFailure()
+    {
+
+        // Non existing
+        $this->json('GET', '/seminar_attendances/9999')
+            ->seeJsonEquals([
+                'status_code' => 404,
+                'status' => 'Not Found',
+                'message' => 'Resource(s) not found',
+            ])
+            ->seeStatusCode(404);
+
+        // Invalid ID
+        $this->json('GET', '/seminar_attendances/abc')
+            ->seeJsonEquals([
+                'status_code' => 400,
+                'status' => 'Bad Request',
+                'message' => 'Request is not valid',
+                'data' => [
+                    'id' => [
+                        'code error_type',
+                        'value abc',
+                        'expected integer',
+                        'used string',
+                        'in path',
+                    ],
+                ]
+            ])
+            ->seeStatusCode(400);
 
     }
     
