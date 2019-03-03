2776f5c32de0073fbbb3f5f50a9125b9ef7c715b
Francesco Zanoni <francescozanoni81@gmail.com>
2019-03-03 15:15:40 +0100

    stage evaluation and interruption report: fixed resources (transformers)

--- app/Http/Resources/Evaluation.php
+++ app/Http/Resources/Evaluation.php
@@ -20,6 +20,12 @@ class Evaluation extends JsonResource
             'id' => $this->id,
             'notes' => $this->notes,
             'stage' => $this->when($this->withStageAttribute($request) === true, new StageResource($this->stage)),
+            
+            // This field is returned as string, but cannot understand why...
+            'clinical_tutor_id' => (int)$this->clinical_tutor_id,
+
+            'created_at' => (string)$this->created_at,
+            'updated_at' => (string)$this->updated_at,
 
         ];
 
