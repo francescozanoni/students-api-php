389066d9a799b7c0ce9313567c6fea48cdb979fd
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2019-03-10 16:51:37 +0100

    stage evaluations > seeder: started reading values from .env

--- database/seeds/EvaluationsTableSeeder.php
+++ database/seeds/EvaluationsTableSeeder.php
@@ -11,20 +11,30 @@ class EvaluationsTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('evaluations')->insert([
+        $record = [
             'stage_id' => 1,
             'clinical_tutor_id' => 456,
             'notes' => 'First evaluation notes',
             'created_at' => '2019-01-25 02:00:00',
             'updated_at' => '2019-01-25 02:00:00',
-        ]);
-        DB::table('evaluations')->insert([
+        ];
+        foreach (config('app.evaluations.items') as $item) {
+            // $record[$item['name']] = $item['values'][0];
+        }
+
+        DB::table('evaluations')->insert($record);
+
+        $record = [
             'stage_id' => 1,
             'clinical_tutor_id' => 789,
             'notes' => 'Second evaluation notes',
             'created_at' => '2019-01-26 02:00:00',
             'updated_at' => '2019-01-26 02:00:00',
             'deleted_at' => '2019-01-26 03:00:00',
-        ]);
+        ];
+        foreach (config('app.evaluations.items') as $item) {
+            // $record[$item['name']] = $item['values'][0];
+        }
+        DB::table('evaluations')->insert($record);
     }
 }
