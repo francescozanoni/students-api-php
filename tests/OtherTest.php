ad3a799a8c2b95f00998e06096e63114b709bca5
Francesco Zanoni <francescozanoni81@gmail.com>
2019-04-13 16:40:42 +0200

    tests: added test of failed validation of response against OpenAPI schema

--- tests/OtherTest.php
+++ tests/OtherTest.php
@@ -33,5 +33,42 @@ class OtherTest extends TestCase
         $this->json('GET', '/url_not_found')
             ->seeStatusCode(404);
     }
+    
+    public function testResponseValidation()
+    {
+    
+        // OpenAPI schema is altered to make response validation fail.
+        $newSchemaFileContent = file_get_contents($this->app['config']['openapi.schema_file_path']);
+        $newSchemaFileContent = str_replace("type: boolean", "type: integer # boolean", $newSchemaFileContent);
+        file_put_contents($this->app['config']['openapi.schema_file_path'], $newSchemaFileContent);
+        
+        $this->json('GET', '/internships/1')
+            ->seeJsonEquals([
+                'status_code' => 500,
+                'status' => 'Internal Server Error',
+                'message' => 'An internal server error occurred',
+                'data' => [
+                    'data.is_optional' => [
+            "code error_type",
+            "value ",
+            "in body",
+            "expected integer",
+            "used boolean"
+        ],
+        "data.is_interrupted" => [
+            "code error_type",
+            "value ",
+            "in body",
+            "expected integer",
+            "used boolean"
+            ]
+                ]
+            ])
+            ->seeStatusCode(500);
+            
+        // Original OpenAPI schema is restored.
+        $newSchemaFileContent = str_replace("type: integer # boolean", "type: boolean", $newSchemaFileContent);
+        file_put_contents($this->app['config']['openapi.schema_file_path'], $newSchemaFileContent);
+    }
 
 }
