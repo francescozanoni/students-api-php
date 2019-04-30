d32d2ec4960664716614ab935610a7c63769830a
Francesco Zanoni <francescozanoni81@gmail.com>
2019-04-30 14:32:59 +0200

    minor improvements

--- app/Http/Middleware/PrettyPrint.php
+++ app/Http/Middleware/PrettyPrint.php
@@ -25,20 +25,20 @@ class PrettyPrint
 
         $response = $next($request);
 
-        if (in_array(app('env'), ['local', 'testing']) === true) {
+        if (in_array(app('env'), ['local', 'testing']) === false) {
+            return $response;
+        }
 
-            // https://www.aaronsaray.com/2017/laravel-pretty-print-middleware
-            if ($response instanceof JsonResponse) {
-                $response->setEncodingOptions(config('app.json_encode_options'));
-            }
-            if ($response instanceof Response &&
-                $response->headers->get('Content-Type') === 'application/json') {
-                $content = $response->getContent();
-                $content = json_decode($content);
-                $content = json_encode($content, config('app.json_encode_options'));
-                $response->setContent($content);
-            }
-
+        // https://www.aaronsaray.com/2017/laravel-pretty-print-middleware
+        if ($response instanceof JsonResponse) {
+            $response->setEncodingOptions(config('app.json_encode_options'));
+        }
+        if ($response instanceof Response &&
+            $response->headers->get('Content-Type') === 'application/json') {
+            $content = $response->getContent();
+            $content = json_decode($content);
+            $content = json_encode($content, config('app.json_encode_options'));
+            $response->setContent($content);
         }
 
         return $response;
