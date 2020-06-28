f472a0d134d86f4cf03f2558e5ab1eb497679a0b
Francesco Zanoni <francesco.zanoni@nubilaria.com>
2020-06-28 11:43:08 +0200

    client: added DataTables of all student's details

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: "./resources/client.js",
     mode: "development",
     output: {
-        path: path.resolve(__dirname, "public"),
+        path: path.resolve(__dirname, "public/client"),
         filename: "lib.js",
         library: "lib",
         libraryTarget: "var",
